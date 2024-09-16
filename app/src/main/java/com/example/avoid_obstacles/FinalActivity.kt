2a16c95e1c544fc5679a90d033e2ea8310d6540c
zivshamli <zivshamli100@gmail.com>
2024-09-16 12:55:17 +0300

    HW2--> Sensor ,Sound , Icon ,Score,Location 2

--- app/src/main/java/com/example/avoid_obstacles/FinalActivity.kt
+++ app/src/main/java/com/example/avoid_obstacles/FinalActivity.kt
@@ -2,7 +2,7 @@ package com.example.avoid_obstacles
 
 import android.os.Bundle
 import android.widget.FrameLayout
-import androidx.activity.enableEdgeToEdge
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
